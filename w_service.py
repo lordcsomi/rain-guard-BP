76381db06b5bd6d475bb5d9e2216e51b1f9e8caa
csomi <csomos.adam.benedek@gmail.com>
2023-10-28 14:20:41 +0200

    v1 server admin

--- w_service.py
+++ w_service.py
@@ -1,9 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, render_template
 from bs4 import BeautifulSoup
 import requests
 import logging
 import datetime
 import sys
+import psutil
+import os
 
 app = Flask("weather")
 
@@ -12,28 +14,27 @@ starting_time = datetime.datetime.now()
 logging.info(f"Starting the weather forecast script at {starting_time}")
 
 URL = "https://www.idokep.hu/elorejelzes/budapest"
-RAINY_HOURS_THRESHOLD = 10
 weather_data_list = []
 
 replace_dict = {
-    "Á" : "A",
-    "á" : "a",
-    "É" : "E",
-    "é" : "e",
-    "Í" : "I",
-    "í" : "i",
-    "Ó" : "O",
-    "ó" : "o",
-    "Ö" : "O",
-    "ö" : "o",
-    "Ő" : "O",
-    "ő" : "o",
-    "Ú" : "U",
-    "ú" : "u",
-    "Ü" : "U",
-    "ü" : "u",
-    "Ű" : "U",
-    "ű" : "u"
+    "Á": "A",
+    "á": "a",
+    "É": "E",
+    "é": "e",
+    "Í": "I",
+    "í": "i",
+    "Ó": "O",
+    "ó": "o",
+    "Ö": "O",
+    "ö": "o",
+    "Ő": "O",
+    "ő": "o",
+    "Ú": "U",
+    "ú": "u",
+    "Ü": "U",
+    "ü": "u",
+    "Ű": "U",
+    "ű": "u"
 }
 
 @app.route('/weather', methods=['GET'])
@@ -63,7 +64,6 @@ def get_weather():
         rain_chance_value = forecast_element.find("div", class_="ik hourly-rain-chance").a.text.strip().split("%")[0] if forecast_element.find("div", class_="ik hourly-rain-chance") else None
 
         # replace Hungarian characters with English ones
-        
         for hungarian, english in replace_dict.items():
             cloudiness = cloudiness.replace(hungarian, english)
             wind = wind.replace(hungarian, english)
@@ -79,14 +79,62 @@ def get_weather():
 
     return jsonify(weather_data_list)
 
-if __name__ == '__main__':
+@app.route('/log', methods=['GET'])
+def get_log():
     try:
-        host = '0.0.0.0'
-        port = 5000
-        print(f"Server IP: {host}, Port: {port}")
-        print(f"Access /weather to get weather data")
-        app.run(host=host, port=port)
+        with open('weather.log', 'r') as log_file:
+            log_data = log_file.read()
+        return log_data
+    except FileNotFoundError:
+        return "Log file not found."
 
-    except (KeyboardInterrupt, SystemExit):
-        print("Received exit command. Exiting...")
-        sys.exit(0)
+@app.route('/admin', methods=['GET', 'POST'])
+def admin_panel():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username == "admin" and password == "password":
+            return render_template('admin.html')
+        else:
+            return "Invalid credentials. Access Denied."
+    return render_template('login.html')
+
+@app.route('/admin/telemetry', methods=['GET'])
+def get_system_performance():
+    try:
+        cpu_usage = psutil.cpu_percent()
+        ram = psutil.virtual_memory().percent
+        disk = psutil.disk_usage('/').percent
+        system_info = {
+            "CPU Usage": f"{cpu_usage}%",
+            "RAM Usage": f"{ram}%",
+            "Disk Usage": f"{disk}%"
+        }
+        return jsonify(system_info)
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+@app.route('/admin/clear-log', methods=['GET'])
+def clear_log():
+    try:
+        open('weather.log', 'w').close()
+        return "Log file cleared."
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+@app.route('/admin/restart-service', methods=['GET'])
+def restart_service():
+    # Code to restart the service goes here
+    return "Service is restarting..."
+
+@app.route('/admin/restart-pi', methods=['GET'])
+def restart_pi():
+    # Code to restart the Raspberry Pi goes here
+    return "Raspberry Pi is restarting..."
+
+if __name__ == '__main__':
+    host = '0.0.0.0'
+    port = 5000
+    print(f"Server IP: {host}, Port: {port}")
+    print(f"Access /weather to get weather data")
+    app.run(host=host, port=port)
